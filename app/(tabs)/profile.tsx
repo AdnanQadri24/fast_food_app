53578ab86219cf0c2968b80a6409453911710fd4
Adnan_Qadri <114274764+AdnanQadri24@users.noreply.github.com>
2025-08-02 16:20:36 +0700

    Update profile.tsx

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,17 @@
-import { View, Text } from 'react-native'
+import { images } from "@/constants";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-center justify-center">
+        <Image source={images.user} className="size-20" tintColor="#000000" />
+        <Text className="font-bold text-xl">Profile</Text>
+        <Text className="text-red-500">*Feature Still Maintenance*</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Profile
+export default Profile;
